a0d0d14c7a1f667be913941f923b302fc95a88db
Omar Rivera <omaration@Omars-MacBook-Air.local>
2025-09-09 13:47:18 -0600

    Cambios en librerias

--- harweb-main/frontend-vue/src/main.js
+++ harweb-main/frontend-vue/src/main.js
@@ -8,6 +8,7 @@ import 'vue-toastification/dist/index.css'
 import './assets/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const app = createApp(App)
 
